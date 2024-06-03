3f4a0a6a5cfcb480e0cc366c685806d30045411a
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-03 15:13:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     preflightContinue: false,
