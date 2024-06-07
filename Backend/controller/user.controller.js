4af11737e8205d709eeb6a5843b45e716dbd5542
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-07 18:59:57 +0530

    Update user.controller.js

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -64,7 +64,7 @@ Your OTP: ${otp}`,
 
 
 if(userdata){
-    sendverifymail(fullname,email,createdUser._id); 
+    await sendverifymail(fullname,email,createdUser._id); 
     //generate a otp and save a document in verify model 
     const createdOtp = new Verify({
         otp: otp,
