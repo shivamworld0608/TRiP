469ed30ad9e507ac319a00b4ae938f9c74754cdb
Shivam Pandey <124254487+shivamworld0608@users.noreply.github.com>
2024-06-07 08:04:09 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -4,7 +4,7 @@ import Login from "./Login";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import toast from "react-hot-toast";
- 
+  
 function Signup() { 
   const [id, setId] = useState(""); 
   const location = useLocation();
